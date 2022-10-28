6075ff080456433d926751c35741365e12d84bbe
gms0817 <gabrielserrano0817@gmail.com>
2022-10-28 14:58:21 -0400

    -Added Sun Valley TTK Theme by rdbende for a more modern feel/appearance -added a few basic responses based on user input -began working on checking if a user exists and creating a user if needed. -Planning to add responses about weather and the user based on information given. -implemented ability to create user and save their data to an encrypted file

--- iris.py
+++ iris.py
@@ -1,30 +1,37 @@
 # Import Statements
 import os
+import pickle
+import random
+from tkinter import ttk
+from datetime import datetime
 import tkinter as tk
-from tkinter import *
-import numpy as np
+import sv_ttk
 import speech_recognition as sr
 import pyttsx3
-import transformers
-import tensorflow
+import numpy as np
+import requests
 
 # -----------------------------------------------------------------------
 # Setup Global-scope Variables
-global output
+global root
 global output_label
 global input_field
 global iris
 global engine
 engine = pyttsx3.init()
+global voice_option
+global user_obj
 
 
 # -----------------------------------------------------------------------
-
-class ChatBotAI():
+# ChatBotAI Setup / Functions && STT/TTS Functions
+class ChatBotAI:
+    global voice_option
+    voice_option = 1
     # Setup / Customize TTS Engine)
     engine.setProperty("rate", 185)
     voices = engine.getProperty('voices')
-    engine.setProperty('voice', voices[1].id)  # voices[0] == male, voices[1] == female
+    engine.setProperty('voice', voices[voice_option].id)  # voices[0] == male, voices[1] == female
 
     def __init__(self, name):
         print("---Loading ", name, "---")
@@ -52,19 +59,48 @@ class ChatBotAI():
         engine.say(text)
         engine.runAndWait()
 
+    @staticmethod
+    def switch_tts_voice(self, option):
+        global voice_option
+        voice_option = option
+        print(voice_option)
+        voices = engine.getProperty('voices')
+        engine.setProperty('voice', voices[voice_option].id)  # voices[0] == male, voices[1] == female
+
 
 def get_response(input_text):
+    # -----------------------------------------------------------------------
+    # Pre-Defined Responses
+    if "time" in input_text:
+        return get_time()
+    elif "bye" in input_text or "see you" in input_text:
+        return np.random.choice(["Have a great day!", "See you next time!", "Goodbye!", "Bye!!"])
+    elif "thank" in input_text:
+        return np.random.choice(["No problem!", "You're welcome!", "Anytime!", "I'm here if you need me!"])
+    elif "your name" in input_text or "who are you" in input_text:
+        return "My name is Iris, and I am your personal assistant!"
+    else:
+        return "I'm sorry. I do not understand."
+
+    # -----------------------------------------------------------------------
+    # AI Generated Responses - WIP
+
+
+def toggle_stt():
+    # Global Variables
     global iris
-    print(input_text)
-    iris.text_to_speech(input_text)
-    return input_text
+    iris.speech_to_text()
 
 
+# -----------------------------------------------------------------------
+# Output Control Functions
 def set_output_text():
     # Global Variables
     global output_label
     global input_field
-    output_label.config(text=get_response(input_field.get()))
+    output = get_response(input_field.get())
+    output_label.config(text=(get_response(output)))
+    iris.text_to_speech(output)
     input_field.delete(0, 'end')  # Empty the input field
 
 
@@ -72,7 +108,9 @@ def set_output_text_voice(speech):
     # Global Variables
     global output_label
     global input_field
-    output_label.config(text=get_response(speech))
+    output = get_response(input_field.get())
+    output_label.config(text=(get_response(output)))
+    iris.text_to_speech(output)
     input_field.delete(0, 'end')  # Empty the input field
 
 
@@ -80,27 +118,110 @@ def set_output_text_key(self):
     # Global Variables
     global output_label
     global input_field
-
-    output_label.config(text=get_response(input_field.get()))
+    output = get_response(input_field.get())
+    output_label.config(text=(get_response(output)))
+    iris.text_to_speech(output)
     input_field.delete(0, 'end')  # Empty the input field
 
 
-def toggle_stt():
+# -----------------------------------------------------------------------
+# Helper Functions
+def get_time():
+    now = datetime.now()
+    current_time = now.strftime("%H:%M:%S")
+    return "The current time is " + current_time
+
+
+# -----------------------------------------------------------------------
+# User Functions
+class User:
+    def __init__(self, user_name, user_zipcode):
+        print("User created. Hello " + user_name)
+        self.user_name = user_name
+        self.user_zipcode = user_zipcode
+
+
+def load_user(filename):
+    try:
+        print(filename)
+        with open(filename, "rb") as f:
+            return pickle.load(f)
+    except Exception as ex:
+        # Create User
+        print("Error: User not found. Calling create_user()")
+        create_user()
+
+
+def save_user(user_obj):
+    try:
+        with open("user.pickle", "wb") as f:
+            pickle.dump(user_obj, f, protocol=pickle.HIGHEST_PROTOCOL)
+    except Exception as ex:
+        print("Success: User has been loaded")
+
+
+def create_user():
+    global user_obj
+    # Launch new user popup
+    new_user_popup()
+
+
+def new_user_popup():
     # Global Variables
-    global iris
+    global root
+    global user_obj
 
-    iris.speech_to_text()
+    # Create popup window
+    new_user_window = tk.Toplevel(root)
+    new_user_window.geometry("400x400")
+    new_user_window.focus()
+
+    # Add entry widget(s) with labels to popup window
+    new_user_label = ttk.Label(new_user_window, text="New User Form")
+    new_user_label.pack()
+
+    user_name_label = ttk.Label(new_user_window, text="Enter your name: ")
+    user_name_label.pack(padx=10, pady=10)
+
+    user_name_field = ttk.Entry(new_user_window, width=25)
+    user_name_field.pack(padx=10, pady=10)
+    user_name_field.focus()
+
+    zipcode_label = ttk.Label(
+        new_user_window,
+        text="Enter your zip-code:\n(Required for location services.)")
+    zipcode_label.pack(padx=10, pady=10)
+    zipcode_field = ttk.Entry(new_user_window, width=25)
+    zipcode_field.pack(padx=10, pady=10)
+
+    # Add submit button to create new user with data provided
+    submit_button = ttk.Button(
+        new_user_window, text="Create User",
+        command=lambda: create_user_obj(user_name_field.get(), zipcode_field.get()))
+    submit_button.pack(padx=10, pady=10, side="bottom")
+
+    def create_user_obj(user_name, zipcode):
+        global user_obj
+        user_obj = User(user_name, zipcode)
+        save_user(user_obj)  # Save user as an object to a picke file
+        print(user_obj.user_name)
+        close_new_user_popup()
+
+    def close_new_user_popup():
+        new_user_window.destroy()
 
 
+# -----------------------------------------------------------------------
+# GUI Window(s) Setup and Functions
 def build_gui():
     # Global Variables
-    global output
     global output_label
     global input_field
+    global root
 
     # Build the user interface
-    # Create instance of tk.Tk class to create application window
-    root = tk.Tk()  # Main Window
+    # Create instance of tk.Tk class to create application window and style
+    root = tk.Tk()  # Main window
     root.title('Iris: Your Personal Assistant')
 
     # Setup window dimensions
@@ -119,54 +240,69 @@ def build_gui():
     root.geometry(f'{window_width}x{window_height}+{center_x}+{center_y}')  # Width, Height, x, y
 
     # Setup header, body, footer, and input frames
-    header_frame = Frame(root, bg="black", width=window_width, height=window_height - 200)
-    header_frame.pack(anchor=N, fill=X, expand=TRUE)
+    header_frame = ttk.Frame(root, width=window_width, height=window_height - 200)
+    header_frame.pack(side="top", fill="x")
 
-    body_frame = Frame(root, width=window_width, height=window_height - 400)
-    body_frame.pack(anchor=CENTER, fill=X, expand=TRUE)
+    body_frame = ttk.Frame(root, width=window_width, height=window_height - 400)
+    body_frame.pack(side="top", fill="x", expand=True)
 
-    footer_frame = Frame(root, bg="black", width=window_width, height=window_height - 200)
-    footer_frame.pack(anchor=S, expand=TRUE, fill=X, side=BOTTOM)
+    footer_frame = ttk.Frame(root, width=window_width, height=window_height - 200)
+    footer_frame.pack(side="bottom", fill="x")
 
-    input_frame = Frame(footer_frame, bg="black")
-    input_frame.pack(anchor=CENTER)
+    input_frame = ttk.Frame(footer_frame)
+    input_frame.pack()
 
     # Add menu bar to header
-    home_button = Menubutton(header_frame, text="Home")
-    home_button.grid(row=0, column=0, padx=10, pady=10)
+    voice_menu_button = ttk.Menubutton(header_frame, text="Voices ")
+    menu = tk.Menu(voice_menu_button, tearoff=0)
+    menu.add_radiobutton(label="David", value="David", command=lambda: iris.switch_tts_voice(0))
+    menu.add_radiobutton(label="Zika", value="Zika", command=lambda: iris.switch_tts_voice(1))
+    voice_menu_button["menu"] = menu
+    voice_menu_button.pack(side="left", padx=10, pady=10)
 
     # Add label to display output
-    output = "Hi, my name is Iris. How may I help you?"
-
-    output_label = tk.Label(body_frame, text=output, font=("Arial", 20), padx=10, pady=10)
+    output_label = ttk.Label(body_frame, text="Hi, my name is Iris. How may I help you?", font=("Arial", 20))
     output_label.pack()
 
     # Add text box to collect user input
-    input_field = tk.Entry(input_frame)
+    input_field = ttk.Entry(input_frame)
     input_field.focus()  # Window will autofocus on Entry widget
-    input_field.pack(side=LEFT, padx=10, pady=10)
+    input_field.pack(side="left", padx=5, pady=10)
 
     input_field.bind('<Return>', set_output_text_key)
 
     # Add button to submit user input for processing
-    submit_button = tk.Button(input_frame, text="Submit", command=set_output_text)
-    submit_button.pack(side=RIGHT, padx=10, pady=10)
+    submit_button = ttk.Button(input_frame, text="Submit", width=8, command=set_output_text)
+    submit_button.pack(side="right", padx=5, pady=10)
 
     # Add toggle to turn on voice recognition
     stt_is_on = True
-    stt_button = tk.Button(input_frame, text="STT", command=toggle_stt)
-    stt_button.pack(side=RIGHT, padx=10, pady=10)
+    stt_button = ttk.Button(input_frame, text="STT", width=4, command=toggle_stt)
+    stt_button.pack(side="right", padx=5, pady=10)
+
+    # Set Sun Valley TTK Theme
+    # Sun Valley TTK Theme by rdbende
+    sv_ttk.set_theme("dark")
+
+    # Load user or create one if needed
+    load_user("user.pickle")
 
     # Keeps window visible on the screen until program is closed
     root.mainloop()
 
+    # Load user, create new one if needed
 
+
+# ------------------------------------------------------------------------
+# Main Function and Program Direction to Main
 def main():
     # Global Variables
     global iris
+
     # Instantiate Iris AI Chatbot
     iris = ChatBotAI(name="Iris")
 
+    # Build main window
     build_gui()
 
 
